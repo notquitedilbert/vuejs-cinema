e3ca1a0fdb553739eccfc64b4e68687f5358ec86
Andrew Shepherd <andrew.shepherd@dwp.gsi.gov.uk>
2017-11-17 16:03:59 +0000

    wip

---
+++ src/main.js
@@ -0,0 +1,49 @@
+import Vue from "vue";
+import './style.scss';
+import genres from './util/genres'
+
+new Vue({
+
+    el: '#app',
+    components: {
+        'movie-list': {
+            template: `<div id="movie-list">
+                        <div v-for="movie in movies" class="movie">
+                            {{movie.title}}
+                        </div>
+                    </div>`,
+            data: function () {
+                return {
+                    movies: [
+                        { title: 'pulp fiction' },
+                        { title: 'home alone' },
+                        { title: 'austin powers' }]
+                }
+            }
+        },
+        'movie-filter': {
+            data() {
+                return { genres }
+            },
+            template:
+                `<div id="movie-filter">
+                    <h2>filter results</h2>
+                    <div class="filter-group">
+                    <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>
+                    </div>
+                </div>`,
+            components: {
+                'check-filter': {
+                    props: ['title'],
+                    template: `<div class="check-filter">
+                        <span class="checkbox"></span>
+                    <span class="check-filter-title">{{title}}</span>
+                            </div>`
+                }
+            }
+        }
+    },
+    data: {
+
+    }
+});
